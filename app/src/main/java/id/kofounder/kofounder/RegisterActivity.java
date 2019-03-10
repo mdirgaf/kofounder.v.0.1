90ee3f020f9bbc2c5b04b45f836cf53abd1a4b25
kofounder <kofounder.info@gmail.com>
2019-03-10 22:06:39 +0800

    Initial commit

--- app/src/main/java/id/kofounder/kofounder/RegisterActivity.java
+++ app/src/main/java/id/kofounder/kofounder/RegisterActivity.java
@@ -1,13 +1,114 @@
 package id.kofounder.kofounder;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText edt_username, edt_fullname, edt_email, edt_password;
+    Button btn_register;
+    TextView txt_login;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edt_username = findViewById(R.id.edt_username);
+        edt_fullname = findViewById(R.id.edt_fullname);
+        edt_email = findViewById(R.id.edt_email);
+        edt_password = findViewById(R.id.edt_password);
+        btn_register = findViewById(R.id.btn_register);
+        txt_login = findViewById(R.id.txt_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        txt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog = new ProgressDialog(RegisterActivity.this);
+                progressDialog.setMessage("Please wait...");
+                progressDialog.show();
+
+                String str_username = edt_username.getText().toString();
+                String str_fullname = edt_fullname.getText().toString();
+                String str_email = edt_email.getText().toString();
+                String str_password = edt_password.getText().toString();
+
+                if (TextUtils.isEmpty(str_username) || TextUtils.isEmpty(str_fullname) || TextUtils.isEmpty(str_email) || TextUtils.isEmpty(str_password)){
+                    Toast.makeText(RegisterActivity.this, "All fields are required!", Toast.LENGTH_SHORT).show();
+                } else if (str_password.length() < 6){
+                    Toast.makeText(RegisterActivity.this, "Password must have 6 characters!", Toast.LENGTH_SHORT).show();
+                } else {
+                    btn_register(str_username, str_fullname, str_email, str_password);
+                }
+            }
+        });
+    }
+
+    public void btn_register(final String edt_username, final String edt_fullname, String edt_email, String edt_password){
+        auth.createUserWithEmailAndPassword(edt_email, edt_password)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userID = firebaseUser.getUid();
+
+                            reference = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("id", userID);
+                            map.put("username", edt_username.toLowerCase());
+                            map.put("fullname", edt_fullname);
+                            map.put("imageurl", "https://firebasestorage.googleapis.com/v0/b/kofounder-kfd01.appspot.com/o/placeholder.png?alt=media&token=29b9705c-8123-4125-b3c2-c91c2f55dbd1");
+                            map.put("bio", "");
+
+                            reference.setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        progressDialog.dismiss();
+                                        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                        } else {
+                            progressDialog.dismiss();
+                            Toast.makeText(RegisterActivity.this, "You can't register with this email or password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
