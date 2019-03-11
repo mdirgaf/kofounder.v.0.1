e59731ce8fb46bd72085dda47588b2ca9398b892
kofounder <kofounder.info@gmail.com>
2019-03-11 11:28:26 +0800

    Initial commit

---
+++ app/src/main/java/id/kofounder/kofounder/Fragments/AkunFragment.java
@@ -0,0 +1,30 @@
+package id.kofounder.kofounder.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.kofounder.kofounder.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AkunFragment extends Fragment {
+
+
+    public AkunFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_akun, container, false);
+    }
+
+}
